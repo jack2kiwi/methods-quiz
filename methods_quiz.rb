68f721185ac10e25067439c030599cba490d1dc3
Jack David Holland <hollandjd@dcsdk12.org>
2016-09-08 13:54:29 -0600

    Implement method_has_teen?

--- methods_quiz.rb
+++ methods_quiz.rb
@@ -1,7 +1,11 @@
 
 # TODO - write has_teen?
 def has_teen?(a, b, c)
-  if a == [13..19]  || b == [13..19] || c == [13..19]
+  if a == 13 || a == 14 ||  a == 15 ||  a == 16 ||  a == 17 ||  a == 18 ||  a == 19
+    true
+  elsif  b == 13 || b == 14 ||  b == 15 ||  b == 16 ||  b == 17 ||  b == 18 ||  b == 19
+    true
+  elsif  c == 13 || c == 14 ||  c == 15 ||  c == 16 ||  c == 17 ||  c == 18 ||  c == 19
     true
   else
     false
